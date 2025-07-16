e92507809c7169693ede851243817aaaae2a2fe5
JosmenDev <josmendev@gmail.com>
2025-07-16 16:14:54 -0500

    feat(participants): added system permisition in endpoints

--- src/external-api/external-api.module.ts
+++ src/external-api/external-api.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { ExternalApiService } from './external-api.service';
 import { ExternalApiController } from './external-api.controller';
 import { ApiKeyGuard } from './guards/api-key.guard';
+import { ProjectModule } from 'src/project/project.module';
+import { SalesModule } from 'src/admin-sales/sales/sales.module';
+import { LeadModule } from 'src/lead/lead.module';
 
 @Module({
+  imports: [
+    ProjectModule,
+    SalesModule,
+    LeadModule,
+  ],
   controllers: [ExternalApiController],
   providers: [ExternalApiService, ApiKeyGuard],
   exports: [ApiKeyGuard],
