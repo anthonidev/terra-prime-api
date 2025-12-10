92108c3574e14ac524412d613dbcf82ca2d903f1
Josmendev <josmendev@gmail.com>
2025-12-10 16:36:12 -0500

    Reinicialización del repositorio

--- src/admin-sales/clients/clients.controller.ts
+++ src/admin-sales/clients/clients.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
 import { User } from 'src/user/entities/user.entity';
 import { GetUser } from 'src/auth/decorators/get-user.decorator';
 import { FindAllClientsDto } from './dto/find-all-clients.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('clients')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles('JVE', 'VEN', 'SCO', 'COB')
 export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
 
